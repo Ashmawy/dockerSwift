d03ba92925e3c3d9e660fa173b79649aee4e3bd8
Mohamed Ashmawy <ashma010@umn.edu>
2016-10-02 12:32:02 -0500

    added sqlit database with images table

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	conn = sqlite3.connect('db.db')
+	return "executed"
 
 if __name__ == "__main__":
     app.run()
