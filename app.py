72a76b786075768eed4505ebdfa8f8f9254d6973
parth017 <arun.parthiban1995@gmail.com>
2016-10-09 12:25:23 -0500

    GET/POST/PUT Done

--- app.py
+++ app.py
@@ -14,12 +14,19 @@ def jsonify_get_images(lst):
 	ret_list = list()
 	for img,logo in lst:
 		ret_list.append({"image": img, "logo": logo})
-
+	print (ret_list)
 	return ret_list
 
-@app.route("/get-images")
-def get_images():
+@app.route("/images",  methods=['POST', 'GET', 'PUT'])
+def images():
+	if request.method == 'GET':
+		return get_images(request)
+	elif request.method == 'PUT':
+		return put_images(request)
+	elif request.method == 'POST':
+		return post_images(request)
 
+def get_images(request):
 	if request.args.get('image'):
 		c = conn.cursor()
 		c.execute('SELECT * FROM images where name=\'' + request.args.get('image') + '\'')
@@ -28,6 +35,20 @@ def get_images():
 		c = conn.cursor()
 		c.execute('SELECT * FROM images')
 		return jsonify(jsonify_get_images(c.fetchall()))
-		
+
+def put_images(request):
+	c = conn.cursor()
+	image = request.json['image']
+	link = request.json['link']
+	c.execute("INSERT INTO images Values( '{image}', '{link}')".format(image=image, link=link))
+	return get_images(request)
+
+def post_images(request):
+	image = request.json['image']
+	return "FROM:"+ image.upper()
+	
+
+
+
 if __name__ == "__main__":
     app.run()
